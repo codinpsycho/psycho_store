0ecb17029900eb786450ed579706bd79e0ff7b71
Ishkaran Singh <ishkaran.singh@hotmail.com>
2016-02-04 23:20:05 +0530

    Updating Media Section
    Adding Youtube link for VGF interview.

--- application/views/basic_view.php
+++ application/views/basic_view.php
@@ -2,7 +2,7 @@
     <h1><?php echo $heading ?> </h1>
     <hr> 
     <div class="well">
-    	<div class="row ">
+    	<div class="row">
 	    	<div class="col-md-12">
 	    		<p><?php echo $content ?></p>
 			</div>
--- application/views/view_media.php
+++ application/views/view_media.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row top-bottom-space">
 	<div class="col-md-4">
 		<a target="_blank" href="http://gamingcentral.in/psycho-store-interview-with-ishkaran-singh/">
 			<img class="img-responsive" src= <?php echo site_url('images/media/gc.jpg')?>  alt="Interview with Psycho Store founder Ishkaran Singh">
@@ -13,4 +13,14 @@
 		<a target="_blank" href="http://gamingcentral.in/psycho-store-interview-with-ishkaran-singh/">Read more </a>
 		</p>
 	</div>
+</div>
+<div class="row">
+	<div class="col-md-5 col-sm-12 col-xs-12">
+		<h4 class="">LXG TV India </h4>
+		<h5>Interview with Ishkaran Singh from Psych Store at VGF 16 Pune : </h5>		
+		<p>So the good guys at LXG TV India (League of extraordinary gamers), quite an extraordinary name for sure, interviewed us at Video Game Fest 16 Pune where we discussed what we do, why we do it and how we will be taking it forward to the next level.
+	</div>
+	<div class="col-md-7 col-sm-12 col-xs-12">		
+		<iframe width="640" height="360" src="https://www.youtube.com/embed/x2wb6NElbGU" frameborder="0" allowfullscreen></iframe>
+	</div>	
 </div>
