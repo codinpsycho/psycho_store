1748961449bbc07948aefc6ea32cfa7737ac74e2
Ishkaran Singh <ishkaran.singh@hotmail.com>
2016-02-14 03:38:32 +0530

    Adding More Partner

--- application/views/view_coupon_partners.php
+++ application/views/view_coupon_partners.php
@@ -1,2 +1,5 @@
 <a target="_blank" href="http://couponclue.com"><img src= <?php echo site_url('images/coupon_partners/couponclue.png')?> ></a>
 <a target="_blank" href="http://couponrani.com"><img src= <?php echo site_url('images/coupon_partners/couponrani.png')?> ></a>
+<a target="_blank" href="https://www.maddycoupons.in/"><img src= <?php echo site_url('images/coupon_partners/maddy-coupons.png')?> ></a>
+<a target="_blank" href="http://picodi.in"><img src= <?php echo site_url('images/coupon_partners/picodi.png')?> ></a>
+<a target="_blank" href="http://couponhaat.in"><img src= <?php echo site_url('images/coupon_partners/couponhaat.png')?> ></a>
