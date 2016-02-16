0934b6f00a1e4b9d8692451c415220bc86577535
Ishkaran Singh <ishkaran.singh@hotmail.com>
2016-02-16 13:22:32 +0530

    Adding Couponzguru

--- application/views/view_coupon_partners.php
+++ application/views/view_coupon_partners.php
@@ -1,5 +1,6 @@
 <a target="_blank" href="http://couponclue.com"><img src= <?php echo site_url('images/coupon_partners/couponclue.png')?> ></a>
-<a target="_blank" href="http://couponrani.com"><img src= <?php echo site_url('images/coupon_partners/couponrani.png')?> ></a>
+<a target="_blank" href="http://www.couponrani.com/psychostore-coupons"><img src= "http://cdn.couponrani.com/assets/images/logo.png" ></a>
 <a target="_blank" href="https://www.maddycoupons.in/"><img src= <?php echo site_url('images/coupon_partners/maddy-coupons.png')?> ></a>
 <a target="_blank" href="http://picodi.in"><img src= <?php echo site_url('images/coupon_partners/picodi.png')?> ></a>
 <a target="_blank" href="http://couponhaat.in"><img src= <?php echo site_url('images/coupon_partners/couponhaat.png')?> ></a>
+<a target="_blank" href="http://www.couponzguru.com/"><img src= "http://cdn3.couponzguru.com/wp-content/uploads/logo.png" ></a>
