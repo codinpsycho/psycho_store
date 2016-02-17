07d189fc50519d7ea5be9ccae605275a6c52bb57
Ishkaran Singh <ishkaran.singh@hotmail.com>
2016-02-17 16:54:43 +0530

    satisfy your inner geek!

--- application/config/app_settings.php
+++ application/config/app_settings.php
@@ -3,8 +3,8 @@
 
 //Default meta tags info
 
-$config['title'] = 'Psycho Store | Stuff for Gamers and Geeks';
-$config['description'] = 'We make stuff for the gaming and geek community of earth(other planets can wait for now)';
+$config['title'] = 'Psycho Store | Satisfy your inner Geek!';
+$config['description'] = 'We make stuff for the gaming and geek community of earth (other planets can wait for now). Get your hands dirty with gaming, anime and geeky t shirts and much more stuff.';
 $config['keywords'] = 't-shirt, tshirt, t shirt, shirt, tee, t, t-shirts, tshirts, t shirts, shirts, tees, ts, clothing, clothes, threads, wear, gift, gifts, hats, hat, beanies, beanie, gear, sweatshirt, hoodie, sweatshirts, hoodies, gamer, geek, hacker, nerd, computer, gamers, geeks, hackers, nerds, coder, coders, psychostore, psycho, store, psycho store, india, ';
 
 $config['favico'] = 'images/favico.png';
@@ -14,7 +14,7 @@ $config['size_chart'] = 'images/size_chart.jpg';
 $config['admin_email'] = array('ishkaran.fearme@gmail.com', 'ishkaran.singh@hotmail.com');
 
 //Mailgun and Newsletter
-$config['mailgun_key'] = 'key-0f80f2f05eb0cc5a7f733be72c8fb5**';	//old one
+$config['mailgun_key'] = 'key-0f80f2f05eb0cc5a7f733be72c8fb5c9';	//old one
 $config['newsletter_address'] = 'updates@news.psychostore.in';
 $config['campaign_id'] = 'psycho_campaign';
 
