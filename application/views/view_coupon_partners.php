0157c141b2cbd0d95c3e1e93506254a7dba9ed9e
Ishkaran Singh <ishkaran.singh@hotmail.com>
2016-02-19 02:19:06 +0530

    Adding Coupondaddy

--- application/views/view_coupon_partners.php
+++ application/views/view_coupon_partners.php
@@ -4,3 +4,4 @@
 <a target="_blank" href="http://picodi.in"><img src= <?php echo site_url('images/coupon_partners/picodi.png')?> ></a>
 <a target="_blank" href="http://couponhaat.in"><img src= <?php echo site_url('images/coupon_partners/couponhaat.png')?> ></a>
 <a target="_blank" href="http://www.couponzguru.com/"><img src= "http://cdn3.couponzguru.com/wp-content/uploads/logo.png" ></a>
+<a target="_blank" href="https://www.coupondaddy.in/stores/psycho-store/"><img src= "https://i2.wp.com/www.coupondaddy.in/wp-content/uploads/2014/02/coupon-daddy-india-logo.png" ></a>
