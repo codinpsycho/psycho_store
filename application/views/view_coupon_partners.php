c9b30bca127014e04facd91de9cbaaee78fb3c8f
Ishkaran Singh <ishkaran.singh@hotmail.com>
2016-02-19 13:05:19 +0530

    Adding frrecouponindia

--- application/views/view_coupon_partners.php
+++ application/views/view_coupon_partners.php
@@ -5,3 +5,4 @@
 <a target="_blank" href="http://couponhaat.in"><img src= <?php echo site_url('images/coupon_partners/couponhaat.png')?> ></a>
 <a target="_blank" href="http://www.couponzguru.com/"><img src= "http://cdn3.couponzguru.com/wp-content/uploads/logo.png" ></a>
 <a target="_blank" href="https://www.coupondaddy.in/stores/psycho-store/"><img src= "https://i2.wp.com/www.coupondaddy.in/wp-content/uploads/2014/02/coupon-daddy-india-logo.png" ></a>
+<a target="_blank" href="http://www.freecouponindia.in/"><img src= <?php echo site_url('images/coupon_partners/freecouponindia.png')?> ></a>
