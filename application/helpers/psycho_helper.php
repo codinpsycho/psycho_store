49c64f8f85b9769d192c66a172438d17eb887104
Ishkaran Singh <ishkaran.singh@hotmail.com>
2016-10-07 16:13:56 +0530

    Correct wrong discount shown
    COD charge needs to be added to the final total amaount.

--- application/helpers/psycho_helper.php
+++ application/helpers/psycho_helper.php
@@ -77,6 +77,8 @@ if(!function_exists('generate_product_table_for_email'))
 			$ci->table->add_row($name,$size,$qty,$price,$total );
 			$final_total += $total;
 		}
+
+		$final_total += $this->config->item('cod_charge');
 		
 		$tmpl = array ( 'table_open'  => '<table border="1" cellpadding="5" cellspacing="0" >' );
 		$ci->table->set_template($tmpl);
