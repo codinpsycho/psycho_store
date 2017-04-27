4adc42d658aeab7309b6cc4a72fd5464c469a04c
Ishkaran Singh <ishkaran.singh@hotmail.com>
2017-04-28 03:08:54 +0530

    Using http link

--- application/views/view_student.php
+++ application/views/view_student.php
@@ -1,1 +1,2 @@
-<iframe src="https://www.frauth.in/v1/frauthembed/58edd9c76ec96df7436a615b" width="100%" height="400px" frameborder="0" style="display: block;">  <p>Frapp</p>  </iframe>
+<iframe src="http://frauth.in/v1/frauthembed/59008d2e73c5b075764975a7
+" width="100%" height="400px" frameborder="0" style="display: block;">  <p>Frapp</p>  </iframe>
