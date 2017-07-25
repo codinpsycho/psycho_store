00c43072d618cbccda47191c5747550504c32ea3
Ishkaran Singh <ishkaran.singh@hotmail.com>
2017-07-26 02:35:43 +0530

    Show 2 items in mobile view

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -10,13 +10,13 @@
             'alt' => "$img_alt",
   );
   ?>
-  	<div class="col-md-4">
+  	<div class="col-md-4 col-xs-6 ">
       <div class="product-link">
       	<?php echo anchor($url, img($image_properties));?>
       	<div class="row">
   	    	<div class="col-md-12 catalog-desc">
   	    		<p class="text-center"> <?php echo $product_item['product_name'] ?>
-             <h4 class="text-center"> <i class="fa fa-rupee"></i> <?php echo $product_item['product_price'] ?></h4></p>
+             <h5 class="text-center"> <i class="fa fa-rupee"></i> <?php echo $product_item['product_price'] ?></h5></p>
   	    	</div>
       	</div>    
       </div>
