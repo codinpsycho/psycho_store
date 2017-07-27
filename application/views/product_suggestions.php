e56bd038170941a5a22fb5d2510ddf9d5621ac90
Ishkaran Singh <ishkaran.singh@hotmail.com>
2017-07-27 17:04:31 +0530

    Use site_url

--- application/views/product_suggestions.php
+++ application/views/product_suggestions.php
@@ -6,7 +6,7 @@
   <div class="col-md-12">
     <?php
     foreach($suggested_products as $product_item):
-      $prod_url = product_url($product_item); 
+      $prod_url = product_url($product_item);
       $path = "/".$product_item['product_image_path'];
       $image_properties = array(
               'src' => "$path",
@@ -14,7 +14,7 @@
     );
     ?>
       <div class="product-link-sm col-md-2 col-sm-4 col-xs-4">
-        <a href= <?php echo $prod_url?> >
+        <a href= <?php echo site_url($prod_url)?> >
           <?php echo img($image_properties);?>
           <p  class="text-center"><?php echo $product_item['product_name']?></p>
         </a>
